921af91696c13a81a4160cfad10cca96fbbc37d1
Henrik Heino <henrik.heino@gmail.com>
2015-03-02 01:52:26 +0200

    Conversion methods ti hex added.

--- django_bytefield.py
+++ django_bytefield.py
@@ -1,13 +1,28 @@
 from django.core import validators
-from django.db.models import Field
+from django.db.models import Field, SubfieldBase
 from django.utils.translation import ugettext_lazy as _
+
+import binascii
 
 class ByteField(Field):
 	description = _("Vector of bytes (up to %(max_length)s)")
 
+	__metaclass__ = SubfieldBase
+
 	def __init__(self, *args, **kwargs):
 		super(ByteField, self).__init__(*args, **kwargs)
 		self.validators.append(validators.MaxLengthValidator(self.max_length))
+
+	def to_python(self, value):
+		if value is None:
+			return None
+		return binascii.hexlify(value)
+
+	def get_prep_value(value, connection):
+		if value is None:
+			return None
+		print value
+		return binascii.unhexlify(value)
 
 	def db_type(self, connection):
 		if connection.settings_dict['ENGINE'] == 'django.db.backends.mysql':
