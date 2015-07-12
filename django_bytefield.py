608ecb8b0156eb8e6dd65dae756cfe8f437dd5f8
Henrik Heino <henrik.heino@gmail.com>
2015-07-12 21:50:53 +0300

    Spaces instead of tabs, because PEP8 says so.

--- django_bytefield.py
+++ django_bytefield.py
@@ -4,28 +4,28 @@ from django.utils.translation import ugettext_lazy as _
 
 import binascii
 
+
 class ByteField(Field):
-	description = _("Vector of bytes (up to %(max_length)s)")
+    description = _("Vector of bytes (up to %(max_length)s)")
 
-	__metaclass__ = SubfieldBase
+    __metaclass__ = SubfieldBase
 
-	def __init__(self, *args, **kwargs):
-		super(ByteField, self).__init__(*args, **kwargs)
-		self.validators.append(validators.MaxLengthValidator(self.max_length))
+    def __init__(self, *args, **kwargs):
+        super(ByteField, self).__init__(*args, **kwargs)
+        self.validators.append(validators.MaxLengthValidator(self.max_length))
 
-	def to_python(self, value):
-		if value is None:
-			return None
-		return binascii.hexlify(value)
+    def to_python(self, value):
+        if value is None:
+            return None
+        return binascii.hexlify(value)
 
-	def get_prep_value(value, connection):
-		if value is None:
-			return None
-		print value
-		return binascii.unhexlify(value)
+    def get_prep_value(value, connection):
+        if value is None:
+            return None
+        return binascii.unhexlify(value)
 
-	def db_type(self, connection):
-		if connection.settings_dict['ENGINE'] == 'django.db.backends.mysql':
-			return 'varbinary(' + str(self.max_length) + ')'
-		else:
-			raise Exception('Only MySQL is currently suppported!')
+    def db_type(self, connection):
+        if connection.settings_dict['ENGINE'] == 'django.db.backends.mysql':
+            return 'varbinary(' + str(self.max_length) + ')'
+        else:
+            raise Exception('Only MySQL is currently suppported!')
