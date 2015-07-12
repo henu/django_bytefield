1e0c61179022a14dd3159af5bc9c30023f8f7928
Henrik Heino <henrik.heino@gmail.com>
2015-07-12 22:22:31 +0300

    Validation fixed.

--- django_bytefield.py
+++ django_bytefield.py
@@ -1,18 +1,30 @@
 from django.core import validators
+from django.core.exceptions import ValidationError
 from django.db.models import Field, SubfieldBase
 from django.utils.translation import ugettext_lazy as _
 
 import binascii
 
 
+def validate_hex(value):
+    if value is None:
+        return
+    if not isinstance(value, basestring):
+        raise ValidationError(_('Not a string!'))
+    for c in value:
+        if c not in '01234567890abcdefABCDEF':
+            raise ValidationError(_('"%s" is not a hex string!') % value)
+
+
 class ByteField(Field):
-    description = _("Vector of bytes (up to %(max_length)s)")
+    description = _('Vector of bytes (up to %(max_length)s)')
 
     __metaclass__ = SubfieldBase
 
     def __init__(self, *args, **kwargs):
         super(ByteField, self).__init__(*args, **kwargs)
-        self.validators.append(validators.MaxLengthValidator(self.max_length))
+        self.validators.append(validators.MaxLengthValidator(self.max_length * 2))
+        self.validators.append(validate_hex)
 
     def from_db_value(self, value, expression, connection, context):
         if value is None:
